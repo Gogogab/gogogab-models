5d4f345a38e1ebe0f7a4bbcdd78d4a8d16c1c0c5
Rahul Devaskar <apostopher@gmail.com>
2015-05-15 07:26:40 +0800

    with more models

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 
 var Star = require('./src/star');
 var Gab = require('./src/gab');
+var User = require('./src/user');
+var FeedItem = require('./src/feeditem');
+var FbUpdate = require('./src/fbupdate')
 
 module.exports = {
   Star: Star,
-  Gab: Gab
+  Gab: Gab,
+  User: User,
+  FeedItem: FeedItem,
+  FbUpdate: FbUpdate
 };
