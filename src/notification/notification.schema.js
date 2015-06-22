bd5c6ffa9559591056aec203ce5c3b53e96c09dc
Rahul Devaskar <apostopher@gmail.com>
2015-06-22 14:57:52 +0800

    updated notification schema

--- src/notification/notification.schema.js
+++ src/notification/notification.schema.js
@@ -3,10 +3,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var NotificationSchema = new Schema({
-  created_at: {type: Date, required: true, default: Date.now, index: true},
+  created_at: {type: Date, required: true, default: Date.now},
   gab: {type: Schema.Types.ObjectId, ref: 'Gab', index: true},
-  provider: {default: '', type: String, index: true},
-  token: String
+  provider: {default: '', type: String},
+  token: {type: String, index: true},
+  failedAttempts: {type: Number, default: 0}
 }, {
   strict: false
 });
