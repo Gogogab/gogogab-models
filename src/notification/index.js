055b71d174281e1d6041216454b574791d6fa046
Rahul Devaskar <apostopher@gmail.com>
2015-06-22 13:55:16 +0800

    with notification

--- src/notification/index.js
+++ src/notification/index.js
@@ -3,4 +3,4 @@
 var mongoose = require('mongoose');
 var NotificationSchema = require('./notification.schema.js');
 
-module.exports = mongoose.model('Gab', NotificationSchema);
+module.exports = mongoose.model('Notification', NotificationSchema);
