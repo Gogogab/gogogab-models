e284c00105cfe83bc7ca32e394b23e84033d1d18
Rahul Devaskar <apostopher@gmail.com>
2015-08-25 16:30:12 +0800

    with random user documents

--- src/user/index.js
+++ src/user/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var random = require('mongoose-random');
 var UserSchema = require('./user.schema.js');
+
+// Attach random plugin. this is used to select random accessTokens.
+UserSchema.plugin(random);
 
 module.exports = mongoose.model('User', UserSchema);
